0c39f35bc5023eceefb499236ceb6015882636f2
isx47262285 <isx47262285@i15.informatica.escoladeltreball.org>
2018-11-07 13:59:44 +0100

    roberto altamirano

---
+++ documentacio_examen.sh
@@ -0,0 +1,7 @@
+# documentacio_examen
+# nombre: roberto altamirano martinez
+# isx: isx47262285
+# edt@ examen ldap
+#########################################################
+
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# README examen
+# nombre: roberto altamirano martinez
+# isx: isx47262285
+# edt@ README ldap
+#########################################################
